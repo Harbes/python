65173a176195bfe7b3357d82cf030cdf5ebc2c00
Harbes <jerryshen68@hotmail.com>
2017-07-21 00:12:09 +0800

    日常更新

--- econometric modelling.py
+++ econometric modelling.py
@@ -50,3 +50,68 @@ print('higher moments:',np.mean(y**2))
 plt.plot(y,label='y')
 plt.legend()
 plt.show()
+
+## example 2.4: bilinear time series
+import numpy as np
+import matplotlib.pyplot as plt
+M=500
+sig=2
+u=np.random.normal(0,sig,M)
+delta=0.95
+y=u[2:]+delta*u[1:-1]*u[:-2]
+print('mean:',np.mean(y))
+print('2nd moment:',np.mean(y**2))
+print('co-2nd moment',np.mean(y[1:]*y[:-1]))
+plt.plot(y)
+plt.show()
+
+
+## example 2.5: exponential distribution
+import numpy as np
+import matplotlib.pyplot as plt
+M=5000
+m=np.empty(M)
+mu=5
+r=0.2
+for i in range(1,M+1):
+    m[i-1]=np.mean(np.random.exponential(mu,i))
+plt.plot(m)
+plt.plot(np.arange(1,M+1),mu*np.ones(M),'k-')
+plt.plot(np.arange(1,M+1),(mu-r)*np.ones(M),'r--')
+plt.plot(np.arange(1,M+1),(mu+r)*np.ones(M),'r--')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
