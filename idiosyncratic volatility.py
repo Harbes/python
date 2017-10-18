ff5fff8b6a18c008075919eab8b01da938a29af1
Harbes <jerryshen68@hotmail.com>
2017-10-18 18:59:39 +0800

    日常更新

--- idiosyncratic volatility.py
+++ idiosyncratic volatility.py
@@ -18,12 +18,12 @@ data=pd.read_pickle('/Users/harbes/data/xccdata/PV')#[['trddt','stkcd','adj_clos
 
 
 #数据处理与删除重复条目
-data['trddt']=pd.to_datetime(data['trddt'].astype(int).astype(str),format='%Y%m%d')
-data=data.set_index(['trddt','stkcd'])
-data.drop_duplicates(subset=None, keep='last',inplace=True)
+#data['trddt']=pd.to_datetime(data['trddt'].astype(int).astype(str),format='%Y%m%d')
+#data=data.set_index(['trddt','stkcd'])
+#data.drop_duplicates(subset=None, keep='last',inplace=True)
 #del data['trddt']
 #del data['stkcd']
-data.sort_index().to_pickle('F:/data/xccdata/PV_datetime')
+#data.sort_index().to_pickle('F:/data/xccdata/PV_datetime')
 
 #计算回报率
 data_adj_close=data['adj_close'].unstack()
@@ -40,11 +40,55 @@ print(time()-now0)#20秒左右
 
 
 
+
+### 用于计算 半方差、方差等指标
 key=lambda x:x.year*100+x.month
 tmp=data_rtn[:100].groupby('key')
 tmp.std()
 
-def semi_variance(arr):
-    return np.sqrt(np.sum((arr-arr.mean())**2*(arr>0))/(len(arr)-1))
+def pos_semi_var(arr):
+    return np.mean((arr-arr.mean())**2* (arr-arr.mean() > 0))
 
-tmp=data_rtn[:100].groupby(key).agg(semi_variance)
+def neg_semi_var(arr):
+    return np.mean((arr - arr.mean()) ** 2 * (arr-arr.mean()< 0))
+def var(arr):
+    return np.mean((arr-arr.mean())**2)
+
+data_rtn[:100].groupby(key).apply(pos_semi_var).iloc[0,0]
+data_rtn[:100].groupby(key).apply(neg_semi_var).iloc[0,0]
+tmp=data_rtn[:500].groupby(key).apply(var)
+tmp.index=pd.to_datetime(tmp.index.values.astype(str),format='%Y%m')
+
+
+
+
+### B/M 中的book 数据
+
+data=pd.read_pickle('F:/data/xccdata/BS')[['fin_year','stkcd','tot_assets','tot_liab']]
+data['fin_year']=pd.to_datetime(data['fin_year'].astype(int).astype(str),format='%Y%m%d')
+data['book']=data['tot_assets']-data['tot_liab']
+from pandas.tseries.offsets import Day,MonthBegin
+data['trddt']=data['fin_year']+MonthBegin()
+data=data.set_index(['trddt','stkcd']).sort_index()
+data=data['book'].unstack()
+tmp=data.resample('M').first().ffill()
+tmp.index=tmp.index-MonthBegin()
+tmp.to_pickle('F:/data/xccdata/essay/book')
+
+
+### market return数据
+data=pd.read_pickle('F:/data/xccdata/index_ret')
+data['trddt']=pd.to_datetime(data['trddt'].astype(int).astype(str),format='%Y%m%d')
+data=data.set_index('trddt')
+data=data.loc[data['index_code']=='399300.SZ']
+data['pctchange'].to_pickle('F:/data/xccdata/essay/index_hs300_daily')
+
+# 月初or月末
+key=lambda x:x.year*100+x.month
+tmp=data['clsprc'].groupby(key)
+tmp=tmp.last()
+tmp.index=pd.to_datetime(tmp.index.values.astype(str),format='%Y%m')
+tmp.to_pickle('F:/data/xccdata/essay/index_hs300_monthend')
+
+
+
