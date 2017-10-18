877b16d76f904485253909e0246a1b478d61ca68
Harbes <jerryshen68@hotmail.com>
2017-10-18 23:03:43 +0800

    日常更新

--- idiosyncratic volatility.py
+++ idiosyncratic volatility.py
@@ -3,6 +3,12 @@ import pandas as pd
 from pandas import DataFrame as DF
 from pandas import Series as SS
 import numpy as np
+
+
+
+from datetime import datetime
+t0=datetime.strptime('2005-02-01','%Y-%m-%d')
+t1=datetime.strptime('2017-09-29','%Y-%m-%d')
 #非必要
 from time import time
 now0=time()
@@ -89,6 +95,188 @@ tmp=data['clsprc'].groupby(key)
 tmp=tmp.last()
 tmp.index=pd.to_datetime(tmp.index.values.astype(str),format='%Y%m')
 tmp.to_pickle('F:/data/xccdata/essay/index_hs300_monthend')
+
+# 计算月度回报
+price0=pd.read_pickle('F:/data/xccdata/essay/index_hs300_monthstart')
+price=pd.read_pickle('F:/data/xccdata/essay/index_hs300_monthend')
+rtn=(price/price.shift(1)-1)*100
+rtn[0]=(price[0]/price0[0]-1)*100
+rtn.to_pickle('F:/data/xccdata/essay/index_hs300_monthly')
+
+
+
+### 计算mimicking portfolio
+
+data=pd.read_pickle('F:/data/xccdata/PV_datetime')[['adj_close','size_free','size_tot']]
+#data=pd.read_pickle('/Users/harbes/data/xccdata/PV')[['trddt','stkcd','adj_close','size_free','size_tot']]
+
+
+#计算股票月度回报率和size月度数据
+data_adj_close=data['adj_close'].unstack()
+key=lambda x:x.year*100+x.month
+tmp=data_adj_close.groupby(key)
+price1=tmp.last();price1.index=pd.to_datetime(price1.index.values.astype(str),format='%Y%m')
+price0=tmp.first();price0.index=pd.to_datetime(price0.index.values.astype(str),format='%Y%m')
+price0.to_pickle('F:/data/xccdata/essay/stocks_clsprc_monthstart')
+price1.to_pickle('F:/data/xccdata/essay/stocks_clsprc_monthend')
+rtn=(price1/price1.shift(1)-1)*100
+rtn.iloc[0]=(price1.iloc[0]/price0.iloc[0]-1)*100
+rtn.to_pickle('F:/data/xccdata/essay/stocks_rtn_monthly')
+
+
+
+size=data['size_tot'].unstack()
+key=lambda x:x.year*100+x.month
+tmp=size.groupby(key)
+size_=tmp.last()
+size_.index=pd.to_datetime(size_.index.values.astype(str),format='%Y%m')
+size_.to_pickle('F:/data/xccdata/essay/stocks_size_tot_monthend')
+
+
+
+
+
+# 计算 mimicking portfolio —— SMB
+rtn=pd.read_pickle('F:/data/xccdata/essay/stocks_rtn_monthly')
+size=pd.read_pickle('F:/data/xccdata/essay/stocks_size_tot_monthend')
+
+group_number=5
+group_label=[i+1 for i in range(group_number)]
+percentile=np.linspace(0,1,group_number+1)
+
+data_group=DF([pd.qcut(size.iloc[i],q=percentile,labels=group_label) for i in range(len(size))],index=size.index,columns=size.columns)
+data_rtn_group=[[rtn.iloc[i+1].loc[data_group.iloc[i]==j+1].sum()/(data_group.iloc[i]==j+1).sum()for j in range(group_number)]for i in range(len(rtn)-1)]
+data_rtn_group=DF(data_rtn_group,index=rtn.index[1:],columns=list('12345'))
+mimick=data_rtn_group['1']-data_rtn_group['5']
+mimick.to_pickle('F:/data/xccdata/essay/SMB_tot')
+#data_rtn_group_sum=DF((np.array(data_rtn_group)+1).cumprod(axis=0),index=rtn.index[1:],columns=list('12345'))
+
+# 计算 mimicking portfolio —— HML
+rtn=pd.read_pickle('F:/data/xccdata/essay/stocks_rtn_monthly')
+size=pd.read_pickle('F:/data/xccdata/essay/stocks_size_free_monthend') # 按总市值做出来的价值因子是编辑显著的
+book=pd.read_pickle('F:/data/xccdata/essay/book')/10000
+BM=book/size
+t0=rtn.index[0]
+BM=BM.loc[t0:]
+
+data_group=DF([pd.qcut(BM.iloc[i],q=percentile,labels=group_label) for i in range(len(BM))],index=BM.index,columns=BM.columns)
+data_rtn_group=[[rtn.iloc[i+1].loc[data_group.iloc[i]==j+1].sum()/(data_group.iloc[i]==j+1).sum()for j in range(group_number)]for i in range(len(rtn)-1)]
+data_rtn_group=DF(data_rtn_group,index=rtn.index[1:],columns=list('12345'))
+mimick=data_rtn_group['5']-data_rtn_group['1']
+mimick.mean()/mimick.std()*np.sqrt(len(mimick))
+mimick.to_pickle('F:/data/xccdata/essay/HML_free')
+
+
+
+
+
+
+# 计算 SMB 和 HML 的日度数据
+data=pd.read_pickle('F:/data/xccdata/PV_datetime')[['adj_close','size_free','size_tot']]
+data_adj_close=data['adj_close'].unstack()
+rtn=(data_adj_close/data_adj_close.shift(1)-1)*100
+size=data['size_free'].unstack()
+
+group_number=5
+group_label=[i+1 for i in range(group_number)]
+percentile=np.linspace(0,1,group_number+1)
+
+data_group=DF([pd.qcut(size.iloc[i],q=percentile,labels=group_label) for i in range(len(size))],index=size.index,columns=size.columns)
+data_rtn_group=[[rtn.iloc[i+1].loc[data_group.iloc[i]==j+1].sum()/(data_group.iloc[i]==j+1).sum()for j in range(group_number)]for i in range(len(rtn)-1)]
+data_rtn_group=DF(data_rtn_group,index=rtn.index[1:],columns=list('12345'))
+mimick=data_rtn_group['1']-data_rtn_group['5']
+mimick.mean()/mimick.std()*np.sqrt(len(mimick))
+mimick.to_pickle('F:/data/xccdata/essay/SMB_free_daily')
+
+
+from datetime import datetime
+t0=datetime.strptime('2005-02-01','%Y-%m-%d')
+t1=datetime.strptime('2017-09-29','%Y-%m-%d')
+book=pd.read_pickle('F:/data/xccdata/essay/book')/10000
+book=book.resample('D').first().ffill()
+book=pd.DataFrame(book,index=data_adj_close.index,columns=data_adj_close.columns)
+book.to_pickle('F:/data/xccdata/essay/book_daily')
+size=data['size_tot'].unstack()
+BM=book/size
+BM=BM.loc[t0:t1]
+rtn=rtn.loc[t0:t1]
+data_group=DF([pd.qcut(BM.iloc[i],q=percentile,labels=group_label) for i in range(len(BM))],index=BM.index,columns=BM.columns)
+data_rtn_group=[[rtn.iloc[i+1].loc[data_group.iloc[i]==j+1].sum()/(data_group.iloc[i]==j+1).sum()for j in range(group_number)]for i in range(len(rtn)-1)]
+data_rtn_group=DF(data_rtn_group,index=rtn.index[1:],columns=list('12345'))
+mimick=data_rtn_group['5']-data_rtn_group['1']
+mimick.mean()/mimick.std()*np.sqrt(len(mimick))
+mimick.to_pickle('F:/data/xccdata/essay/HML_tot_daily')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 计算 三因子 alpha
+
+import pandas as pd
+from pandas import DataFrame as DF
+from pandas import Series as SS
+import numpy as np
+from numpy.linalg import pinv
+
+from datetime import datetime
+t0=datetime.strptime('2005-02-01','%Y-%m-%d')
+t1=datetime.strptime('2017-09-29','%Y-%m-%d')
+
+data_adj_close=pd.read_pickle('F:/data/xccdata/PV_datetime')[['adj_close']].unstack()
+rtn=(data_adj_close/data_adj_close.shift(1)-1)*100
+SMB=pd.read_pickle('F:/data/xccdata/essay/SMB_tot_daily') #153
+SMB.name='SMB'
+HML=pd.read_pickle('F:/data/xccdata/essay/HML_tot_daily')  #153
+HML.name='HML'
+market=pd.read_pickle('F:/data/xccdata/essay/index_hs300_daily')  #154
+market.name='market'
+
+
+c=pd.Series(1,index=market.index)
+X=pd.concat([c,market,SMB,HML],axis=1)
+X=X.loc[t0:t1]
+rtn=rtn.loc[t0:t1]
+rtn.index=(rtn.index.year).astype(str)+'-'+(rtn.index.month).astype(str).str.zfill(2)
+rtn.index.name='date'
+rtn=rtn.stack()
+#rtn['date']=(rtn.index.get_level_values(0).year).astype(str)+'-'+(rtn.index.get_level_values(0).month).astype(str).str.zfill(2)
+#rtn=rtn.set_index(['date',rtn.index.get_level_values(1)])
+
+def func(arr):
+    z= arr.values!=np.nan
+    if z.sum()<=10:
+        return 0.0
+    else:
+        x=(X[arr.index.get_level_values(0)[0]]).values[z,:]
+        return pinv(x.T@x)@x.T@(arr.values[z])[0]
+alpha=rtn[:100].groupby(['date','stkcd']).apply(func)
+
+
+rtn.index.get_level_values(0).year
+
+
+
+
+
 
 
 
