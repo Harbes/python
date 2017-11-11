bdbc1e575ae6f97a34f12a85853c9c3bae4eb0d0
harbes <jerryshen68@hotmail.com>
2017-11-12 00:21:17 +0800

    日常更新

--- quant_illiquidity.py
+++ quant_illiquidity.py
@@ -52,13 +52,20 @@ size=data['size_tot'].unstack().groupby(key).mean()
 size.index = pd.to_datetime(size.index.values.astype(str),format=('%Y%m'))
 
 mark_size=DataFrame([pd.qcut(size.iloc[i],q=percentile,labels=label_size) for i in range(len(size))],index=size.index)
-mark_illiq=DataFrame([pd.qcut(illiq_m.iloc[i],q=percentile,labels=label_illiq) for i in range(len(illiq_m))],index=illiq_m.index)
+mark_illiq=DataFrame(np.nan,index=illiq_m.index,columns=illiq_m.columns)
 
+for l_i in label_illiq:
+    tmp=DataFrame([pd.qcut(illiq_m.iloc[i][mark_size.iloc[i]==l_i],q=percentile,labels=label_illiq) for i in range(len(illiq_m))],index=illiq_m.index)
+    mark_illiq=mark_illiq.combine_first(tmp)
 
+size_illiquidity=DataFrame(np.zeros((25,len(size)-1)),index=pd.MultiIndex.from_product([label_size,label_illiq]))
+for s in label_size:
+    for i in label_illiq:
+        mark_illiq_tmp=DataFrame
+        size_illiquidity.loc[(s,i),:]= pd.Series([rtn.iloc[j][mark_size.iloc[j-1]==s][mark_illiq.iloc[j-1]==i].mean() for j in range(1,len(rtn))])
 
-
-
-
+# 结果显示
+(size_illiquidity+1).loc[(4,slice(None)),slice(None)].T.cumprod().plot() # 为什么使用axis=1不能得到想要的结果？？？
 
 
 
