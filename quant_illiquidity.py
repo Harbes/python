f0178450a0eeec5f81417fdd1b08d15e8d3479a1
harbes <jerryshen68@hotmail.com>
2017-11-12 08:36:02 +0800

    日常更新

--- quant_illiquidity.py
+++ quant_illiquidity.py
@@ -23,6 +23,9 @@ illiq=np.abs((clsprc-opnprc)/opnprc/amount)
 illiq_m=illiq.groupby(key).mean()
 illiq_m.index = pd.to_datetime(illiq_m.index.values.astype(str),format=('%Y%m'))
 
+
+
+
 group_num=10
 percentile=np.linspace(0,1,group_num+1)
 label_illiq=[i+1 for i in range(group_num)] # 1表示流动性较好的组合，另一个极端组合是流动性较差的组合
@@ -42,6 +45,8 @@ size.index = pd.to_datetime(size.index.values.astype(str),format=('%Y%m'))
 size=DataFrame([[size.iloc[i][mark_illiq.iloc[i]==k].mean() for k in label_illiq] for i in range(len(size))],index=size.index,columns=label_illiq)
 size.mean()/1e5
 
+
+
 # double-sort,first by size,then by illiq
 group_num=5
 percentile=np.linspace(0,1,group_num+1)
@@ -53,20 +58,22 @@ size.index = pd.to_datetime(size.index.values.astype(str),format=('%Y%m'))
 
 mark_size=DataFrame([pd.qcut(size.iloc[i],q=percentile,labels=label_size) for i in range(len(size))],index=size.index)
 mark_illiq=DataFrame(np.nan,index=illiq_m.index,columns=illiq_m.columns)
-
 for l_i in label_illiq:
     tmp=DataFrame([pd.qcut(illiq_m.iloc[i][mark_size.iloc[i]==l_i],q=percentile,labels=label_illiq) for i in range(len(illiq_m))],index=illiq_m.index)
     mark_illiq=mark_illiq.combine_first(tmp)
 
 size_illiquidity=DataFrame(np.zeros((25,len(size)-1)),index=pd.MultiIndex.from_product([label_size,label_illiq]))
+s_i_count=DataFrame(np.zeros((25,len(size)-1)),index=pd.MultiIndex.from_product([label_size,label_illiq])) # 用于检查分组是否均匀
 for s in label_size:
     for i in label_illiq:
         mark_illiq_tmp=DataFrame
         size_illiquidity.loc[(s,i),:]= pd.Series([rtn.iloc[j][mark_size.iloc[j-1]==s][mark_illiq.iloc[j-1]==i].mean() for j in range(1,len(rtn))])
+        s_i_count.loc[(s, i), :]=pd.Series([rtn.iloc[j][mark_size.iloc[j-1]==s][mark_illiq.iloc[j-1]==i].count() for j in range(1,len(rtn))])
 
 # 结果显示
 (size_illiquidity+1).loc[(4,slice(None)),slice(None)].T.cumprod().plot() # 为什么使用axis=1不能得到想要的结果？？？
 
+j=1;s=1;i=1
+rtn.iloc[j][mark_size.iloc[j-1]==s][mark_illiq.iloc[j-1]==i]#.count()
 
-
-
+(s_i_count.max()-s_i_count.min()).max()
