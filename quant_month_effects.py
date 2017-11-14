48a7097c3c85f6953bbf0c59f601c9d5d932c84c
harbes <jerryshen68@hotmail.com>
2017-11-15 01:21:41 +0800

    日常更新

--- quant_month_effects.py
+++ quant_month_effects.py
@@ -4,6 +4,9 @@ from pandas import DataFrame
 from datetime import datetime
 import matplotlib.pyplot as plt
 
+t1=10
+t0=-5
+
 # 整理数据
 #data=pd.read_pickle('F:/data/xccdata/PV_datetime')[['adj_open','adj_close']]
 data=pd.read_pickle('/Users/harbes/data/xccdata/PV_datetime')[['amount','opnprc','clsprc','adj_open','adj_close','size_tot','size_free']]
@@ -39,11 +42,11 @@ plt.plot((rtn_feb.mean(axis=1)+1).values.cumprod())
 #rtn_feb.mean(axis=1).groupby(rtn_feb.index.year).describe()
 
 # 春节后n个有效交易日
-rtn_after_fes=pd.concat([rtn.iloc[rtn.index.get_loc(i[0])-3:rtn.index.get_loc(i[0])+8] for i in after_fes_data])
+rtn_after_fes=pd.concat([rtn.iloc[rtn.index.get_loc(i[0])+t0:rtn.index.get_loc(i[0])+t1] for i in after_fes_data])
 plt.plot((rtn_after_fes.mean(axis=1)+1).values.cumprod(axis=0))
 plt.show()
 # 春节前n个有效交易日
-rtn_before_fes=pd.concat([rtn.iloc[rtn.index.get_loc(i[0])-18:rtn.index.get_loc(i[0])-3] for i in after_fes_data])
+rtn_before_fes=pd.concat([rtn.iloc[rtn.index.get_loc(i[0])+t0-10:rtn.index.get_loc(i[0])+t1-10] for i in after_fes_data])
 plt.plot((rtn_before_fes.mean(axis=1)+1).values.cumprod(axis=0))
 
 tmp=rtn_after_fes.mean(axis=1);tmp.mean()/tmp.std()*np.sqrt(len(tmp))
@@ -62,10 +65,10 @@ mark_size=DataFrame([pd.qcut(size.iloc[i],q=percentile,labels=label_size) for i 
 rtn_by_size=DataFrame([[rtn.iloc[i].loc[mark_size.iloc[i-1]==k].mean() for k in label_size] for i in range(1,len(rtn))],index=rtn.index[1:],columns=label_size)
 
 # 春节后
-rtn_after_fes=pd.concat([rtn_by_size.iloc[rtn_by_size.index.get_loc(i[0])-5:rtn_by_size.index.get_loc(i[0])+10] for i in after_fes_data])
+rtn_after_fes=pd.concat([rtn_by_size.iloc[rtn_by_size.index.get_loc(i[0])+t0:rtn_by_size.index.get_loc(i[0])+t1] for i in after_fes_data])
 plt.plot((rtn_after_fes+1).values.cumprod(axis=0))
 # 春节前
-rtn_before_fes=pd.concat([rtn_by_size.iloc[rtn_by_size.index.get_loc(i[0])-10:rtn_by_size.index.get_loc(i[0])-5] for i in after_fes_data])
+rtn_before_fes=pd.concat([rtn_by_size.iloc[rtn_by_size.index.get_loc(i[0])+t0-15:rtn_by_size.index.get_loc(i[0])+t1-15] for i in after_fes_data])
 plt.plot((rtn_before_fes+1).values.cumprod(axis=0))
 
 
@@ -125,12 +128,12 @@ opnprc=opnprc.loc[indi.index]
 clsprc=clsprc.loc[indi.index]
 mark_=DataFrame([pd.qcut(indi.iloc[np.maximum(i-30,0):i].mean(),q=percentile,labels=label_) for i in range(1,len(indi))],index=indi.index[1:],columns=indi.columns)
 for k,i in enumerate(after_fes_data):
-    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+10]-opnprc.iloc[opnprc.index.get_loc(i[0])-5])/opnprc.iloc[opnprc.index.get_loc(i[0])-5]
+    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+t1]-opnprc.iloc[opnprc.index.get_loc(i[0])+t0])/opnprc.iloc[opnprc.index.get_loc(i[0])+t0]
     for j in range(num_by_):
-        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])]==j+1].mean()
+        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])+t0]==j+1].mean()
 plt.plot((rtn_after_fes+1).values.cumprod(axis=0))
 tmp=rtn_after_fes[10]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp))
-
+tmp=rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp))
 
 
 
@@ -148,9 +151,9 @@ opnprc=opnprc.loc[indi.index]
 clsprc=clsprc.loc[indi.index]
 mark_=DataFrame([pd.qcut(indi.iloc[np.maximum(i-30,0):i].mean(),q=percentile,labels=label_) for i in range(1,len(indi))],index=indi.index[1:],columns=indi.columns)
 for k,i in enumerate(after_fes_data):
-    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+10]-opnprc.iloc[opnprc.index.get_loc(i[0])-5])/opnprc.iloc[opnprc.index.get_loc(i[0])-5]
+    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+t1]-opnprc.iloc[opnprc.index.get_loc(i[0])+t0])/opnprc.iloc[opnprc.index.get_loc(i[0])+t0]
     for j in range(num_by_):
-        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])]==j+1].mean()
+        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])+t0]==j+1].mean()
 plt.plot((rtn_after_fes+1).values.cumprod(axis=0))
 tmp=rtn_after_fes[10]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp))
 
@@ -158,7 +161,7 @@ tmp=rtn_after_fes[10]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp))
 
 
 
-# 按past分组(前n个交易日数据) ; y  ; 资本流入 or 流出 ？; 中国是contrarian 策略有效，但是春节是确实动量策略有效
+# 按past分组(前n个交易日数据) ; y  ; 资本流入 or 流出 ？;
 num_by_=10
 label_=[i+1 for i in range(num_by_)] #
 percentile=np.linspace(0,1,num_by_+1)
@@ -171,9 +174,9 @@ opnprc=opnprc.loc[indi.index]
 clsprc=clsprc.loc[indi.index]
 mark_=DataFrame([pd.qcut(indi.iloc[np.maximum(i-30,0):i].mean(),q=percentile,labels=label_) for i in range(1,len(indi))],index=indi.index[1:],columns=indi.columns)
 for k,i in enumerate(after_fes_data):
-    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+10]-opnprc.iloc[opnprc.index.get_loc(i[0])-5])/opnprc.iloc[opnprc.index.get_loc(i[0])-5]
+    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+t1]-opnprc.iloc[opnprc.index.get_loc(i[0])+t0])/opnprc.iloc[opnprc.index.get_loc(i[0])+t0]
     for j in range(num_by_):
-        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])]==j+1].mean()
+        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])+t0]==j+1].mean()
 plt.plot((rtn_after_fes+1).values.cumprod(axis=0))
 tmp=rtn_after_fes[10]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp))
 
@@ -181,7 +184,9 @@ tmp=rtn_after_fes[10]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp))
 
 
 
-# 按price分组(前n个交易日数据) ; y【low price在中国是不显著的，春节期间却是显著的】
+# 按price分组(前n个交易日数据) ; y【low price在中国是不显著的，春节期间却是显著的，但是不稳定，对策略构建的日期较为敏感; 但是春节后显著】
+t0=0
+t1=9
 num_by_=10
 label_=[i+1 for i in range(num_by_)] #
 percentile=np.linspace(0,1,num_by_+1)
@@ -192,14 +197,16 @@ opnprc=opnprc.loc[indi.index]
 clsprc=clsprc.loc[indi.index]
 mark_=DataFrame([pd.qcut(indi.iloc[np.maximum(i-30,0):i].mean(),q=percentile,labels=label_) for i in range(1,len(indi))],index=indi.index[1:],columns=indi.columns)
 for k,i in enumerate(after_fes_data):
-    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+10]-opnprc.iloc[opnprc.index.get_loc(i[0])-5])/opnprc.iloc[opnprc.index.get_loc(i[0])-5]
+    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+t1]-opnprc.iloc[opnprc.index.get_loc(i[0])+t0])/opnprc.iloc[opnprc.index.get_loc(i[0])+t0]
     for j in range(num_by_):
-        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])]==j+1].mean()
+        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])+t0]==j+1].mean()
 plt.plot((rtn_after_fes+1).values.cumprod(axis=0))
-tmp=rtn_after_fes[10]-rtn_after_fes[2];tmp.mean()/tmp.std()*np.sqrt(len(tmp)) # 注意，此处是第10组减去第2组
+tmp=rtn_after_fes[10]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp)) #
+rtn_after_fes.mean()
 
 
-# 按波动率分组(前n个交易日数据) ; y
+
+# 按波动率分组(前n个交易日数据) ;  不显著！！！
 num_by_=10
 label_=[i+1 for i in range(num_by_)] #
 percentile=np.linspace(0,1,num_by_+1)
@@ -210,16 +217,16 @@ opnprc=opnprc.loc[indi.index]
 clsprc=clsprc.loc[indi.index]
 mark_=DataFrame([pd.qcut(rtn.iloc[np.maximum(i-30,0):i].std(),q=percentile,labels=label_) for i in range(5,len(indi))],index=indi.index[5:],columns=indi.columns)
 for k,i in enumerate(after_fes_data):
-    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+10]-opnprc.iloc[opnprc.index.get_loc(i[0])-5])/opnprc.iloc[opnprc.index.get_loc(i[0])-5]
+    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+t1]-opnprc.iloc[opnprc.index.get_loc(i[0])+t0])/opnprc.iloc[opnprc.index.get_loc(i[0])+t0]
     for j in range(num_by_):
-        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])]==j+1].mean()
+        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])+t0]==j+1].mean()
 plt.plot((rtn_after_fes+1).values.cumprod(axis=0))
-tmp=rtn_after_fes[9]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp)) #
+tmp=rtn_after_fes[10]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp)) #
 tmp=rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp)) #
 rtn_after_fes.mean()
 
 
-# 按delta_price * size 分组(前n个交易日数据) ; 非线性关系：组合收益先增后减
+# 按delta_price * size 分组(前n个交易日数据) ; 非线性关系：组合收益先增后减；但是多空组合显著
 num_by_=10
 label_=[i+1 for i in range(num_by_)] #
 percentile=np.linspace(0,1,num_by_+1)
@@ -233,18 +240,18 @@ opnprc=opnprc.loc[indi.index]
 clsprc=clsprc.loc[indi.index]
 mark_=DataFrame([pd.qcut(indi.iloc[np.maximum(i-30,0):i].mean(),q=percentile,labels=label_) for i in range(1,len(indi))],index=indi.index[1:],columns=indi.columns)
 for k,i in enumerate(after_fes_data):
-    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+10]-opnprc.iloc[opnprc.index.get_loc(i[0])-5])/opnprc.iloc[opnprc.index.get_loc(i[0])-5]
+    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+t1]-opnprc.iloc[opnprc.index.get_loc(i[0])+t0])/opnprc.iloc[opnprc.index.get_loc(i[0])+t0]
     for j in range(num_by_):
-        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])]==j+1].mean()
+        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])+t0]==j+1].mean()
 plt.plot((rtn_after_fes+1).values.cumprod(axis=0))
-tmp=rtn_after_fes[5]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp))
+tmp=rtn_after_fes[10]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp))
 
 rtn_after_fes.mean(axis=0)
 
 
 
 
-# 按delta_price * 资金净流入 分组(前n个交易日数据) ;
+# 按delta_price * 资金净流入 分组(前n个交易日数据) ; 非线性
 num_by_=10
 label_=[i+1 for i in range(num_by_)] #
 percentile=np.linspace(0,1,num_by_+1)
@@ -258,9 +265,9 @@ opnprc=opnprc.loc[indi.index]
 clsprc=clsprc.loc[indi.index]
 mark_=DataFrame([pd.qcut(indi.iloc[np.maximum(i-30,0):i].mean(),q=percentile,labels=label_) for i in range(1,len(indi))],index=indi.index[1:],columns=indi.columns)
 for k,i in enumerate(after_fes_data):
-    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+10]-opnprc.iloc[opnprc.index.get_loc(i[0])-5])/opnprc.iloc[opnprc.index.get_loc(i[0])-5]
+    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+t1]-opnprc.iloc[opnprc.index.get_loc(i[0])+t0])/opnprc.iloc[opnprc.index.get_loc(i[0])+t0]
     for j in range(num_by_):
-        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])]==j+1].mean()
+        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])+t0]==j+1].mean()
 plt.plot((rtn_after_fes+1).values.cumprod(axis=0))
 tmp=rtn_after_fes[10]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp))
 
@@ -270,7 +277,7 @@ rtn_after_fes.mean(axis=0)
 
 
 
-# 按 signed-volume 分组(前n个交易日数据) ; y (前10个交易日资金净流入)
+# 按 signed-volume 分组(前n个交易日数据) ; N
 num_by_=10
 label_=[i+1 for i in range(num_by_)] #
 percentile=np.linspace(0,1,num_by_+1)
@@ -284,39 +291,12 @@ opnprc=opnprc.loc[indi.index]
 clsprc=clsprc.loc[indi.index]
 mark_=DataFrame([pd.qcut(indi.iloc[np.maximum(i-15,0):i].mean(),q=percentile,labels=label_) for i in range(1,len(indi))],index=indi.index[1:],columns=indi.columns)
 for k,i in enumerate(after_fes_data):
-    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+10]-opnprc.iloc[opnprc.index.get_loc(i[0])-5])/opnprc.iloc[opnprc.index.get_loc(i[0])-5]
+    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+t1]-opnprc.iloc[opnprc.index.get_loc(i[0])+t0])/opnprc.iloc[opnprc.index.get_loc(i[0])+t0]
     for j in range(num_by_):
-        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])]==j+1].mean()
+        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])+t0]==j+1].mean()
 plt.plot((rtn_after_fes+1).values.cumprod(axis=0))
 tmp=rtn_after_fes[10]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp))
 rtn_after_fes.mean(axis=0)
-
-
-
-
-
-# 按 size_free 分组(前n个交易日数据) ; y (前10个交易日资金净流入)
-num_by_=10
-label_=[i+1 for i in range(num_by_)] #
-percentile=np.linspace(0,1,num_by_+1)
-rtn_after_fes=DataFrame(0,index=np.array(after_fes_data)[:,0],columns=label_)
-opnprc=data['adj_open'].unstack()[filter_==1]
-clsprc=data['adj_close'].unstack()[filter_==1]
-size=data['size_free'].unstack()[filter_==1]
-indi=size-size.shift(1)
-indi.drop_duplicates(keep=False,inplace=True) # 有一些日期全是nan，需要剔除
-opnprc=opnprc.loc[indi.index]
-clsprc=clsprc.loc[indi.index]
-mark_=DataFrame([pd.qcut(indi.iloc[np.maximum(i-15,0):i].mean(),q=percentile,labels=label_) for i in range(1,len(indi))],index=indi.index[1:],columns=indi.columns)
-for k,i in enumerate(after_fes_data):
-    rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+10]-opnprc.iloc[opnprc.index.get_loc(i[0])-5])/opnprc.iloc[opnprc.index.get_loc(i[0])-5]
-    for j in range(num_by_):
-        rtn_after_fes.iloc[k,j]=rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])]==j+1].mean()
-plt.plot((rtn_after_fes+1).values.cumprod(axis=0))
-tmp=rtn_after_fes[10]-rtn_after_fes[1];tmp.mean()/tmp.std()*np.sqrt(len(tmp))
-
-rtn_after_fes.mean(axis=0)
-
 
 
 
@@ -338,7 +318,7 @@ after_formation=[]
 for jj in range(1,4):
     rtn_after_fes = DataFrame(0, index=np.array(after_fes_data)[:, 0], columns=label_)
     for k,i in enumerate(after_fes_data[:]):
-        rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+8+20*jj]-opnprc.iloc[opnprc.index.get_loc(i[0])+20*jj-12])/ opnprc.iloc[opnprc.index.get_loc(i[0])+30*jj-20]
+        rtn_tmp=(clsprc.iloc[clsprc.index.get_loc(i[0])+t1+1+20*jj]-opnprc.iloc[opnprc.index.get_loc(i[0])+20*jj+t1-19])/ opnprc.iloc[opnprc.index.get_loc(i[0])+20*jj+t1-19]
         for j in range(num_by_):
             rtn_after_fes.iloc[k,j]=rtn_tmp.mean() # rtn_tmp[mark_.iloc[mark_.index.get_loc(i[0])]==j+1].mean()
     after_formation.append(rtn_after_fes)
@@ -375,11 +355,12 @@ indi_2=data['size_tot'].unstack()[filter_==1]
 indi_2=indi_2.loc[indi.index]
 
 
-mark_1=DataFrame([pd.qcut(indi_2.iloc[np.maximum(i-30,0):i].mean(),q=percentile,labels=label_) for i in range(1,len(indi_1))],index=indi_1.index[1:],columns=indi_1.columns)
+mark_1=DataFrame([pd.qcut(indi_1.iloc[np.maximum(i-30,0):i].mean(),q=percentile,labels=label_) for i in range(1,len(indi_1))],index=indi_1.index[1:],columns=indi_1.columns)
 #mark_1=DataFrame([pd.qcut((clsprc.iloc[i-1]-opnprc.iloc[np.maximum(i-30,0)])/opnprc.iloc[np.maximum(i-30,0)],q=percentile,labels=label_) for i in range(1,len(indi_1))],index=indi_1.index[1:],columns=indi_1.columns)
 mark_2=DataFrame(np.nan,index=mark_1.index,columns=mark_1.columns)
+# 下面这一小段代码有问题，tmp、mark_2、indi_2的shape不同
 for l_ in label_:
-    tmp=DataFrame([pd.qcut(indi_1.iloc[np.maximum(i-30,0):i].mean()[mark_1.iloc[i-1]==l_],q=percentile,labels=label_) for i in range(1,len(indi_2))],index=indi_2.index[1:])
+    tmp=DataFrame([pd.qcut(indi_2.iloc[np.maximum(i-30,0):i].mean()[mark_1.iloc[i-1]==l_],q=percentile,labels=label_) for i in range(1,len(indi_2))],index=indi_2.index[1:])
     mark_2=mark_2.combine_first(tmp)
 
 
@@ -389,8 +370,7 @@ for s in label_:
     for i in label_:
         for j in after_fes_data:
             rtn_2_sort.loc[(s,i),j[0]]= \
-                ((clsprc.iloc[clsprc.index.get_loc(j[0])+8]-opnprc.iloc[clsprc.index.get_loc(j[0])-3])/opnprc.iloc[clsprc.index.get_loc(j[0])-3])\
-                    [np.logical_and(mark_1.iloc[mark_1.index.get_loc(j[0])]==s,mark_2.iloc[mark_2.index.get_loc(j[0])]==i)].mean()
+                ((clsprc.iloc[clsprc.index.get_loc(j[0])+t1]-opnprc.iloc[opnprc.index.get_loc(j[0])+t0])/opnprc.iloc[opnprc.index.get_loc(j[0])+t0])[np.logical_and(mark_1.iloc[mark_1.index.get_loc(j[0])+t0]==s,mark_2.iloc[mark_2.index.get_loc(j[0])+t0]==i)].mean()
 
 # 结果显示
 (rtn_2_sort+1).loc[(1,slice(None)),slice(None)].T.cumprod().plot() # 为什么使用axis=1不能得到想要的结果？？？
@@ -399,11 +379,11 @@ tmp=rtn_2_sort.loc[(1,1)]-rtn_2_sort.loc[(1,5)];tmp.mean()/tmp.std()*np.sqrt(len
 
 
 
+clsprc.iloc[clsprc.index.get_loc(after_fes_data[-2][0])+t1]-opnprc.loc['2016-02-15']
 
+mark_2.loc['2016-02-15'].mean()
 
-
-
-
+after_fes_data[-2]
 
 
 
