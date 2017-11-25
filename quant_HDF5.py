03aa41960ad4b3a2230b6cd7f80df25d14d51652
Harbes <jerryshen68@hotmail.com>
2017-11-25 09:42:43 +0800

    日常更新

--- quant_HDF5.py
+++ quant_HDF5.py
@@ -19,7 +19,9 @@ store.close()
 
 
 import h5py
-filename = '/Users/harbes/data/xccdata/bid_ask/20150225'
+#filename = '/Users/harbes/data/xccdata/bid_ask/20150225.h5'
+filename = 'F:/data/xccdata/bid_ask/20150225.h5'
+
 f = h5py.File(filename, 'r')
 #f.name
 #list(f.keys())
@@ -38,13 +40,12 @@ import pandas as pd
 import os
 import h5py
 from time import time
-
 import warnings
 warnings.filterwarnings("ignore")
+
 #rootdir = '/Users/harbes/data/xccdata/bid_ask'
 rootdir = 'F:/data/xccdata/bid_ask'
 li_ = [i for i in os.listdir(rootdir) if not i.endswith('_') and not i.endswith('.h5')] #列出文件夹下所有的目录与文件
-
 n_stock=4000
 n_obs=20
 n_indi=30
@@ -53,18 +54,18 @@ for i in li_[:1]: # li_[1:]: #
     path = rootdir+'/'+i
     #if os.path.isfile(path):
     f = h5py.File(path, 'r')
-    #os.mkdir(rootdir + '/' + i + '_hdf5')
+    os.mkdir(rootdir + '/' + i + '_')
     for stock in list(f['stk'])[0:n_stock]:
         data=pd.DataFrame((list(f['stk'][stock][indi][-n_obs:]) for indi in list(f['stk'][stock])[:n_indi]),index=list(f['stk'][stock])[:n_indi])
-        #data.to_pickle(rootdir + '/' + i +'_'+ '/' +stock)
-        data.to_hdf(rootdir + '/' + i +'.h5',stock,format='table',mode='w')
+        data.to_pickle(rootdir + '/' + i +'_'+ '/' +stock)
+        #data.to_hdf(rootdir + '/' + i +'.h5',stock,format='table',append=True) # 速度慢，体积大
     f.close()
 print(time()-now0)
 
 
 
 
-
+data=pd.read_hdf(rootdir+'/20150225.h5','000001')
 
 
 
