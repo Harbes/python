0cd35a125228b6b1e025ee35c378051081c1266f
harbes <jerryshen68@hotmail.com>
2017-12-07 18:46:51 +0800

    日常更新

--- quant_share_holding_structure.py
+++ quant_share_holding_structure.py
@@ -1,13 +1,26 @@
 import pandas as pd
+import numpy as np
 from pandas.tseries.offsets import YearEnd
 
-insti_holding = pd.read_csv('/Users/harbes/data/xccdata/insti_holding.csv').iloc[1:].set_index(['Date', 'SCode'])
+insti_holding = pd.read_csv('/Users/harbes/data/xccdata/insti_holding.csv').iloc[1:]
+insti_holding['Date'] = pd.to_datetime(insti_holding['Date'])
+insti_holding = insti_holding.set_index(['Date', 'SCode'])
 insti_holding.index.names = ['trddt', 'stkcd']
+insti_holding = insti_holding.sum(axis=1)
+# insti_holding=insti_holding.rename('shares')
+insti_holding = insti_holding.unstack()
 insti_holding.head()
 
 govern_holding = pd.read_csv('/Users/harbes/data/xccdata/govern_holding.csv').iloc[1:]
-govern_holding['Year'] = govern_holding['Year'].to_datetime() + YearEnd()
-govern_holding.set_index(['Year', ''])
+govern_holding['Year'] = pd.to_datetime(govern_holding['Year']) + YearEnd()
+govern_holding = govern_holding.set_index(['Year', 'SCode'])
+govern_holding = govern_holding.unstack()
+govern_holding.columns = govern_holding.columns.get_level_values(1)
+govern_holding.index.names = ['trddt']
+govern_holding.columns.names = ['stkcd']
 govern_holding.head()
+govern_holding[govern_holding.isnull()] = np.nan
+govern_holding = govern_holding.astype(np.float64)
 
-govern_holding.index.get_level_values(0) = govern_holding.index.get_level_values(0).to_datetime() + YearEnd()
+non_individual = govern_holding + insti_holding
+individual = 100 - non_individual
