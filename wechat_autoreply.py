7572e0d46f0fd9ffed36ad8b95515f28b0693e2f
Harbes <jerryshen68@hotmail.com>
2018-03-07 08:14:02 +0800

    日常更新

--- wechat_autoreply.py
+++ wechat_autoreply.py
@@ -4,7 +4,7 @@
 import itchat
 @itchat.msg_register(itchat.content.TEXT)
 def print_content(msg):
-    return msg.user.send('对不起，您联系的用户暂时不在线。如有急事，请联系：15600927092；jerryshen68@hotmail.com')
+    return msg.user.send('【自动回复】对不起，您联系的用户暂时不在微信旁。如有急事，请联系：15600927092；jerryshen68@hotmail.com')
 itchat.auto_login(hotReload=True)#,enableCmdQR = True)
 itchat.run()
 
