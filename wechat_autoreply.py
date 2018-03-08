bda5e17c4964ed2327dcc5c3747ecaa108c1279f
Harbes <jerryshen68@hotmail.com>
2018-03-08 20:31:56 +0800

    日常更新

--- wechat_autoreply.py
+++ wechat_autoreply.py
@@ -3,8 +3,13 @@
 
 import itchat
 to_place=input('Where to go: ')
-if to_place in ['健身房','0'] :
-    send_message='【自动回复】对不起，我去健身了，暂时无法回复。'
+contact=input('leave contact message or not:')
+if set(contact) & set(('t','T','y','Y','是')):
+    leave_message=True
+else:
+    leave_message=False
+if to_place is not '':
+    send_message='【自动回复】对不起，我去'+to_place+'了，暂时无法回复!'+leave_message*' 如有急事，请联系：15600927092；jerryshen68@hotmail.com'
 else:
     send_message='【自动回复】对不起，您联系的用户暂时不在微信旁。如有急事，请联系：15600927092；jerryshen68@hotmail.com'
 @itchat.msg_register(itchat.content.TEXT)
