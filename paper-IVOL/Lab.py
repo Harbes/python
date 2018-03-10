cc9a0478a9d5b083e5e3f5051af02f915b2dde81
Harbes <jerryshen68@hotmail.com>
2018-03-10 14:58:03 +0800

    日常更新

--- paper-IVOL/Lab.py
+++ paper-IVOL/Lab.py
@@ -12,7 +12,7 @@ from scipy.stats import skew,kurtosis
 def import_data():
     global data_path,pv,open_price,close_price,index_ret,rtn
     data_path = 'E:/data/NewData/'  # '/Users/harbes/data/NewData/' #
-    pv = pd.read_pickle(data_path + 'PV_datetime')[['adj_close', 'adj_open', 'size_tot']]
+    pv = pd.read_pickle(data_path + 'PV_datetime')#[['adj_close', 'adj_open', 'size_tot']]
     close_price = pv['adj_close'].unstack()
     # close_price.index=pd.to_datetime(close_price.index.astype(int).astype(str),format='%Y%m%d')
     open_price = pv['adj_open'].unstack()
@@ -20,11 +20,30 @@ def import_data():
     index_ret = pd.read_pickle(data_path + 'index_ret').set_index(['index_code', 'trddt'])['pctchange'].loc['000016.SH']
     index_ret.index = pd.to_datetime(index_ret.index.astype(int).astype(str), format='%Y%m%d')
     rtn = (close_price - open_price) / open_price * 100
+    # 注意，rtn的最后两列是index return 和 月份序号
     rtn['index'] = index_ret  # ['pctchange']
-    # 注意，rtn的最后一列是月份序号
+
     rtn['month'] = (rtn.index.year - rtn.index[0].year) * 12 + rtn.index.month
     index_ret = pd.DataFrame({'pctchange': index_ret,
                               'month': (index_ret.index.year - index_ret.index[0].year) * 12 + index_ret.index.month})
+def import_book():
+    '''
+    注意：book是由assets-liability得来的，数据中有负数存在
+    monthly
+    :return:
+    '''
+    BS = pd.read_pickle(data_path + '/BS')[['fin_year', 'stkcd', 'tot_assets', 'tot_liab']].set_index(
+        ['fin_year', 'stkcd']).sort_index()
+    book = BS['tot_assets'] - BS['tot_liab']
+    book = book.drop(book.index[book.index.duplicated(keep='last')]).unstack() * 1e-8
+    book.index = pd.to_datetime(book.index.astype(str), format='%Y%m%d')
+    book.index.name = 'trddt'
+    book = book.resample('M').first().ffill()
+    # 下面这一小段代码似乎不需要，只是为了防止日期不是月末
+    By=lambda x:x.year*100+x.month
+    book=book.groupby(By).nth(0)
+    book.index= pd.to_datetime(book.index.astype(str), format='%Y%m')+MonthEnd()
+    return book[book>0]
 
 
 def cal_beta(periods,save_data=False):
@@ -45,22 +64,7 @@ def cal_beta(periods,save_data=False):
         beta.to_pickle(data_path + 'beta_daily_'+str(periods)+'M')
     else:
         return beta
-
-def import_book():
-    '''
-    注意：book是由assets-liability得来的，数据中有负数存在
-    monthly
-    :return:
-    '''
-    BS = pd.read_pickle(data_path + '/BS')[['fin_year', 'stkcd', 'tot_assets', 'tot_liab']].set_index(
-        ['fin_year', 'stkcd']).sort_index()
-    book = BS['tot_assets'] - BS['tot_liab']
-    book = book.drop(book.index[book.index.duplicated(keep='last')]).unstack() * 1e-8
-    book.index = pd.to_datetime(book.index.astype(str), format='%Y%m%d')
-    book.index.name = 'trddt'
-    book = book.resample('M').first().ffill()
-    return book[book>0]
-def import_size():
+def cal_size(save_data=False):
     '''
     monthly
     :return:
@@ -68,20 +72,20 @@ def import_size():
     GroupBy = lambda x: x.year * 100 + x.month
     size = pv['size_tot'].unstack().groupby(GroupBy).last()*1e-4
     size.index = pd.to_datetime(size.index.astype(str), format='%Y%m') + MonthEnd()
-    return size
-def cal_size_BM(save_data=False):
-    book=import_book()
-    size=import_size()
-    BM=book/size
     if save_data:
         size.to_pickle(data_path+'size_monthly')
+    return size
+def cal_BM(save_data=False):
+    book=import_book()
+    size=cal_size()
+    BM=book/size
+    if save_data:
         BM.to_pickle(data_path+'BM_monthly')
     else:
-        return size,BM
+        return BM
 def cal_mom(periods,save_data=False):
     GroupBy = lambda x: x.year * 100 + x.month
     cls=close_price.groupby(GroupBy).nth(-1)
-    #cls.index=pd.to_datetime(cls.index.astype(str),format='%Y%m')
     opn=open_price.groupby(GroupBy).nth(0)
     mom=(cls.shift(1)-opn.shift(periods))/opn.shift(periods)*100
     mom.index = pd.to_datetime(mom.index.astype(str), format='%Y%m')+MonthEnd()
@@ -99,3 +103,39 @@ def cal_rev(save_data=False):
         rev.to_pickle(data_path+'Reversal_M')
     else:
         return rev
+def cal_illiq(periods,save_data=False):
+    GroupBy = lambda x: x.year * 100 + x.month
+    amount=pv['amount'].unstack()
+    rtn_amount=1e5*np.abs(close_price-open_price)/open_price/amount
+    rtn_amount_sum=rtn_amount.groupby(GroupBy).sum()
+    rtn_amount_count=rtn_amount.groupby(GroupBy).count()
+    _sum=0;_count=0;
+    for i in range(periods):
+        _sum +=rtn_amount_sum.shift(i)
+        _count +=rtn_amount_count.shift(i)
+    illiq=_sum/_count
+    illiq.index=pd.to_datetime(illiq.index.astype(str),format='%Y%m')+MonthEnd()
+    illiq.iloc[:periods-1]=np.nan
+    if save_data:
+        illiq.to_pickle(data_path+'illiq_'+str(periods)+'M')
+    else:
+        return illiq
+def cal_skew(periods,save_data=False):
+    t_skew=pd.DataFrame(index=pd.date_range('20050101','20180301',freq='M'),columns=rtn.columns)
+    for i in range(periods,159):
+        t_skew.iloc[i-1]=rtn[(rtn['month']>=i-periods+1)&(rtn['month']<=i)].iloc[:,:-2].skew()
+    if save_data:
+        t_skew.to_pickle(data_path+'skew_'+str(periods)+'M')
+    else:
+        return t_skew
+def cal_coskew():
+    pass
+def cal_iskew():
+    pass
+def cal_vol():
+    pass
+def cal_vol_ss():
+    pass
+def cal_ivol():
+    pass
+tmp=cal_skew(10);tmp.head(10)
