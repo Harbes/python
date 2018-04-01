56a5baec8b9ca73d16147a1660734031d99c2b8d
Harbes <jerryshen68@hotmail.com>
2018-04-01 16:02:40 +0800

    日常更新

--- FromPapers.py
+++ FromPapers.py
@@ -15,13 +15,16 @@ def SizeAndValueInChina_LiuStambaughYuan():
     var_dict=GetVarsFromList(var_list,freq)
     filter_=FilterSample(var_dict['size'],percentile=[0.0,0.3,1.0],label=[0.0,1.0])
     var_dict['ret']=var_dict['ret'][filter_]
-    res=DoublePortAnalysis(['turnover'],'ivol',var_dict=var_dict)
+    res1_EW=SinglePortAnalysis(['ivol'],var_dict=var_dict,freq=freq)
+    res1_VW = SinglePortAnalysis(['ivol'], var_dict=var_dict, freq=freq,value_weighted=True)
+    res2_EW=DoublePortAnalysis(['turnover'],'ivol',var_dict=var_dict)
+    res2_VW = DoublePortAnalysis(['turnover'], 'ivol', var_dict=var_dict,value_weighted=True)
     FM_res=Fama_MacBeth(var_list,freq=freq,var_dict=var_dict)
-    return res,FM_res
+    return res1_EW,res1_VW,res2_EW,res2_VW,FM_res,var_dict
 
 
 
 if __name__ is '__main__':
     t0=time()
-    res_2,FM_res=SizeAndValueInChina_LiuStambaughYuan()
-    print(time()-t0)
+    res1_EW, res1_VW, res2_EW, res2_VW, FM_res,var_dict=SizeAndValueInChina_LiuStambaughYuan()
+    print(time()-t0) # 450s
