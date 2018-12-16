f79aa1b3ec4a95427636b0f482440421408b7bf7
harbes <jerryshen68@hotmail.com>
2018-12-16 16:06:59 +0800

    daily updates for OptionMatrix

--- OptionMatrix/StochasticAnalysis.py
+++ OptionMatrix/StochasticAnalysis.py
@@ -4,9 +4,10 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.stats import norm
 
 # Chapter 3. the Lebesgue Integral
+## Cantor set
 np.set_printoptions(precision=5)
 def CantorSet():
     k=6
@@ -14,12 +15,27 @@ def CantorSet():
     Cantor=np.zeros(M)
     for i in range(M):
         for j in range(k):
-            Cantor[i]=round((Cantor[i]+2.0*np.random.uniform())/3.0,5)
+            Cantor[i]=round((Cantor[i]+2.0*np.random.uniform())/3.0,5) #理论上，ln(BinsNumber)/ln(10)=< prec <= ln(3)/ln(10)*k
     return Cantor
-#from decimal import *
-#getcontext().prec = 5 # ln(BinsNumber)/ln(10)=< prec <= ln(3)/ln(10)*k
-
 Cantor=CantorSet()
-plt.hist(Cantor,3**10);plt.show()
+plt.hist(Cantor,3**10);plt.show() # 结果似乎不是很理想
 
-np.log(3)/np.log(10)*7
+
+# Chapter 4. Basic Probability Theory
+## Simulation 4.2 (equally speced points in the unit interval)
+N=100
+X=np.random.randn(N)
+Y=np.sort(X)
+U=np.arange(1/(N+1),1.0,1/(N+1))
+V=norm.cdf(Y)
+x=np.arange(0,1.0,0.01)
+plt.plot(x,x);plt.plot(V,U,'o');plt.show()
+## Simulation 4.3 (Quantile-Quantile plot)
+N=100
+X=np.random.randn(N)
+Y=np.sort(X)
+U=np.arange(1/(N+1),1.0,1/(N+1))
+V=norm.ppf(U)
+x=np.arange(-4,4,0.02)
+plt.plot(x,x);plt.plot(Y,V,'o');plt.show()
+
