a51514e917086bced95391e940045b0a4294221b
Harbes <jerryshen68@hotmail.com>
2019-09-19 01:06:45 +0800

    日常更新

--- RiskBasedAndFactorInvesting.py
+++ RiskBasedAndFactorInvesting.py
@@ -20,9 +20,26 @@ std_mvp=np.sqrt(1/SIGMA_inv.sum())
 from scipy.optimize import minimize,Bounds,LinearConstraint
 bounds=Bounds(np.zeros(4),np.ones(4))
 matrix_LC=np.array([np.ones(4),r_mean])
-linear_constraint=LinearConstraint(matrix_LC,[1.0,0.03],[1.0,0.05])
+linear_constraint=LinearConstraint(matrix_LC,[1.0,r_p],[1.0,r_p]) #两个r_p换成一个range后，minimize报错
 def func(W):
     return (W*SIGMA*W).sum()
 r_p=0.04
 W0=np.ones(4)/4
-res=minimize(func,W0,method='trust-constr',constraints=linear_constraint,bounds=bounds)
+res=minimize(func,W0,method='trust-constr',constraints=linear_constraint,bounds=bounds)# 权重总是不满足约束条件，不知为何
+# 使用cvxopt包
+from cvxopt import matrix,solvers
+
+P=matrix(SIGMA,tc='d') #二次项
+q=matrix([0]*4,tc='d') #一次项
+G=matrix(np.diag([-1]*4),tc='d')#不等式约束(全部改写成<=)
+h=matrix([0]*4,tc='d')
+A=matrix(np.vstack(([1]*4,r_mean)),tc='d')#等式约束
+n=100
+sigmas=np.zeros(n)
+r_p=0.03+np.arange(n)*.0003
+for i in range(n):
+    b=matrix([1.0,r_p[i]],tc='d')
+    sol=solvers.qp(P,q,G,h,A,b)
+    sigmas[i]=np.sqrt(sol['primal objective'])
+import matplotlib.pyplot as plt
+plt.plot(sigmas*100,r_p*100);plt.show()
