7814d4e0552570f9545f55d5e155065138024bf5
Harbes <jerryshen68@hotmail.com>
2019-11-10 23:57:34 +0800

    daily update

---
+++ Lab-IPCA.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+from scipy.sparse.linalg import svds
+from pandas.tseries.offsets import DateOffset
+DPath='E:/data/CNRDS/'
+indicators_all=pd.read_pickle(DPath+'indicators_all')
+PV=pd.read_pickle(DPath+'PV');PV.tail(10)
+tmp=PV[['Scode','Trddt','Adclsprc']].iloc[1:].set_index(['Trddt','Scode'])
+tmp=tmp.astype(float)
+adjprc=tmp.loc[~tmp.index.duplicated(keep='last')].unstack() # 提出duplicates
+adjprc.index=pd.to_datetime(adjprc.index,format='%Y-%m-%d')# 调整index格式
+adjprc.columns=adjprc.columns.get_level_values(1)# 调整columns格式
+GroupBy1=lambda x:x.year*100.0+x.month
+#p0=adjprc.groupby(by=GroupBy).first();p0
+ret=adjprc.groupby(by=GroupBy1).last().pct_change();
+ret.index=pd.to_datetime(ret.index.astype(int).astype(str),format='%Y%m')
+
+# 标准化indicators
+indicators_all.index.names=['Trddt','Scode']
+indi_standardized=indicators_all.groupby(by=['Trddt']).apply(lambda x:(x-x.mean())/x.std())
+indi_standardized['ret']=ret.stack().drop_duplicates(keep='last')
+X_tmp=indi_standardized.iloc[:,:-1].mul(indi_standardized['ret'],axis=0) # 个股characteristics与return的乘积
+Nts=(X_tmp.sum(axis=1)!=0).groupby('Trddt').sum()
+X=X_tmp.groupby('Trddt').sum().div(Nts,axis=0) ## 部分数值为0，原因在于原始predictor可能都是nan【为什么predictor会出现大面积且连续的NaN？】;删与不删对结果影响几何？稳健吗？
+indi_standardized=indi_standardized.loc[X_tmp.sum(axis=1)!=0].fillna(0.0)
+W=pd.DataFrame(index=pd.MultiIndex.from_product([Nts.index,indi_standardized.columns[:-1]], names=['Trddt','predictors']),columns=indi_standardized.columns[:-1])
+for i in Nts.index:
+    W.loc[i]=(indi_standardized.loc[i,:'SI'].T@indi_standardized.loc[i,:'SI']/Nts.loc[i]).values
+
+#Initial guess
+K=2
+Gamma_Old,s,v     = sp.sparse.linalg.svds(X.T,K);
+Factor_Old          = s*v.T
+Factor_Old.shape
+def num_IPCA_estimate_ALS(Gamma_Old,W,X,Nts,PSF=None):
+
+    '''
+    % usage:
+    % -[ Gamma_New , F_New ] = num_IPCA_estimate_ALS( Gamma_Old , W , X , Nts )
+    % -[ Gamma_New , F_New ] = num_IPCA_estimate_ALS( Gamma_Old , W , X , Nts , PSF )
+    % inputs
+    % - Gamma_Old : LxK matrix of previous iteration's GammaBeta estimate
+    % - W : LxLxT array of Z(:,:,t)'*Z(:,:,t)/Nts(t)
+    % - X : LxT array of Z(:,:,t)'*Y(:,t)/Nts(t)
+    % - Nts : 1xT vector of cross-sectional size [typically found as sum(LOC)]
+    % - (optional) PSF : Kadd x T Pre-Specified Factors
+    %
+    % outputs
+    % - Gamma_New : LxK matrix of this iteration's GammaBeta estimate
+    % - F_New : KxT matrix of this iteration's Factor estimate
+    %
+    % Imposes identification assumption on Gamma_New and F_New: Gamma_New is orthonormal
+    % matrix and F_New has positive mean (taken across the T columns)
+    %
+    % When nargin>4, estimates a LxKtilde Gamma_New and a Ktilde x T F_New, where
+    % Ktilde=K+Kadd
+    % The first K columns of Gamma_New is GammaBeta.
+    % The last Kadd columns of Gamma_New is GammaDelta.
+    % We continue to only return the KxT factor estimate in F_New
+    %
+    % When nargin<=4, Ktilde=K and GammaDelta is not estimated
+    '''
+    T=len(Nts)
+    if PSF is not None:
+        L,K_tilde=np.shape(Gamma_Old)
+        K=K_tilde-K_add
+    else:
+        L, K_tilde = np.shape(Gamma_Old)
+        K=K_tilde
+    F_New=np.empty(K,T)
+    if K>0:
+        if PSF is not None:
+            for t in range(T):
+                F_New[:,t]=np.linalg.pinv(Gamma_Old[:,:K].T@W[:,:,t]@Gamma_Old[:,:K])@Gamma_Old[:,:K].T@\
+                           (X[:,t]-W[:,t]@Gamma_Old[:,K:]*PSF[t])
+        else:
+            for t in range(T):
+                F_New[:,t]=np.linalg.pinv(Gamma_Old.T@W[:,:,t]@Gamma_Old)@Gamma_Old.T@X[:,t]
+    Numerator=np.zeros(L*K_tilde,1)
+    Denominator=np.zeros(L*K_tilde,1)
+    if PSF is not None:
+
+    return None
+
+
