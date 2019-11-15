7820fe2520a3561572fd64131d70e8727d7ad32c
Harbes <jerryshen68@hotmail.com>
2019-11-15 13:28:25 +0800

    daily update

--- my_modules/EconTool.py
+++ my_modules/EconTool.py
@@ -16,7 +16,7 @@ def OLS(Y,X,constant_included=True,print_res='params'):
     n=3000
     rand=np.random.randn(n)
     a=np.kron(spector_data.endog,rand).reshape(32,n)
-    pd.DataFrame(a).apply(OLS_simple)
+    pd.DataFrame(a).apply(OLS_simple) # 对于多个参数的func，可以使用pipe
     :param Y:
     :param X:
     :param constant_included:
@@ -40,3 +40,17 @@ def OLS(Y,X,constant_included=True,print_res='params'):
         return res.params
 
 
+data = sm.datasets.longley.load()
+data.exog = sm.add_constant(data.exog)
+ols_resid = sm.OLS(data.endog, data.exog).fit().resid
+res_fit = sm.OLS(ols_resid[1:], ols_resid[:-1]).fit()
+rho = res_fit.params;rho
+from scipy.linalg import toeplitz
+order = toeplitz(np.arange(16));order
+sigma = rho**order
+gls_model = sm.GLS(data.endog, data.exog, sigma=sigma)
+ols_model = sm.GLS(data.endog, data.exog)
+gls_results = gls_model.fit();gls_results.params
+ols_model.fit().params
+sm.OLS(data.endog, data.exog).fit().params
+print(gls_results.summary())
